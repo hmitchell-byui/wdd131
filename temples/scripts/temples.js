9c78557b0df9b5f4fb1329d269512d15a606cd79
hmitchell-byui <hm1tch3ll99@gmail.com>
2025-01-30 16:30:19 -0600

    Almost there

--- temples/scripts/temples.js
+++ temples/scripts/temples.js
@@ -2,3 +2,10 @@ let d = new Date();
 document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()}`;
 document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
 
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
