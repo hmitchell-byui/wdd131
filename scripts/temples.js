c2e166ed56a20e78d85e67f80773485444f289b9
hmitchell-byui <hm1tch3ll99@gmail.com>
2025-02-03 17:02:43 -0600

    almost?

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,12 +2,9 @@ let d = new Date();
 document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()}`;
 document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
 
-document.addEventListener('resize', function(){
-    const hamButton = document.getElementById('menu');
-    const navigation = document.getElementById('nav-menu');
-
-    hamButton.addEventListener('click', function() {
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+hamButton.addEventListener('click', function() {
         navigation.classList.toggle('open');
         hamButton.classList.toggle('open');
     });
-});
